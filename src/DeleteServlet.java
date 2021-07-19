7acf4c01a9c1208e7dec8407c30e027f8dc91748
sinchana M K <mksinchanasonu@gmail.com>
2021-07-19 10:51:06 +0530

    servlet

---
+++ src/DeleteServlet.java
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="orange">
+<form method="post" action="fs">
+<table>
+<tr><td>ID:</td>
+<td><input type="text" name="i"/>
+</td></tr>
+<tr><td>NAME:</td>
+<td><input type="text" name="nm"/>
+</td></tr>
+<tr><td>Percentage:</td>
+<td><input type="text" name="pr"/>
+</td></tr>
+<tr><td>Department:</td>
+<td><input type="text" name="dp"/>
+</td></tr>
+<tr>
+<td><input type="submit" value="post"/></td></tr>
+
+</table>
+
+</form>
+
+
+</body>
+</html>
